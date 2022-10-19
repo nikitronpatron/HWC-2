b385ccff7158c0c82ba4c97a07ace5c1db0cb20f
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-19 22:40:32 +0300

    EX13

---
+++ EX13/Program.cs
@@ -0,0 +1,19 @@
+﻿// На ввод: любое число
+// На вывод: третья цифра либо предупреждение, что ее нет
+
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99)
+{
+    while(number > 1000)
+    {
+        number = number / 10;
+    }
+    int thirdNum = number % 10;
+    Console.WriteLine(thirdNum);
+}
+else
+{
+    Console.WriteLine("There is no third digit");
+}
