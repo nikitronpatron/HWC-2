30802d09a943e92912b6e8b288005eef6263ef63
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-19 22:10:17 +0300

    EX10

---
+++ EX10/Program.cs
@@ -0,0 +1,22 @@
+﻿// На ввод: трехзначное число
+// На вывод: вторую цифру числа
+
+Console.WriteLine("Enter three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int ten = number / 10;
+    int hundred = number / 100;
+    int result = ten - hundred * 10;
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(result);
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Error");
+    Console.ResetColor();
+}
