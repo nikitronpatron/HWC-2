61fcde9c9185ce4a037c61002c332305a5892d1e
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-19 22:14:36 +0300

    EX15

---
+++ EX15/Program.cs
@@ -0,0 +1,33 @@
+﻿// На ввод: цифра, обозначающая день недели
+// На вывод: является ли этот день недели выходным (да/нет)
+
+Console.WriteLine("Enter a number from 1 to 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("no");
+        break;
+    case 2:
+        Console.WriteLine("no");
+        break;
+    case 3:
+        Console.WriteLine("no");
+        break;
+    case 4:
+        Console.WriteLine("no");
+        break;
+    case 5:
+        Console.WriteLine("no");
+        break;
+    case 6:
+        Console.WriteLine("yes");
+        break;
+    case 7:
+        Console.WriteLine("yes");
+        break;
+    default:
+        Console.WriteLine("Error");
+        break;
+}
